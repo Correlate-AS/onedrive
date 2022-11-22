c6c2bf866986e0545e31f9b5f96075a6eadacd0a
Maria Mikhalska <rolana93@gmail.com>
2022-11-22 14:17:11 +0200

    - new sharepoint methods: getFileById, getPublicUrl; - add new base class for drives to avoid cod duplicates of getFileById and getPreview #48-3

--- src/oneDriveClient.js
+++ src/oneDriveClient.js
@@ -8,14 +8,15 @@ const {
     validateAndDefaultTo,
     getParamValue,
 } = require("./util");
+const BaseDriveClient = require('./baseDriveClient');
+
 
 const ROOT_URL = 'https://graph.microsoft.com/v1.0';
 const rootFolderId = 'root';
-class OneDriveClient {
+class OneDriveClient extends BaseDriveClient {
 
     constructor(graphApi, logger) {
-        this.graphApi = graphApi;
-        this.logger = logger;
+        super(graphApi, logger);
     }
 
     shareTo(fileId, driveId, email) {
@@ -90,18 +91,7 @@ class OneDriveClient {
 
     getFileById(fileId) {
         this.logger.info('Getting OneDrive file', { fileId });
-        return this.graphApi.request(`https://graph.microsoft.com/v1.0/me/drive/items/${fileId}`)
-        .catch(logErrorAndReject(`Non-200 while querying file ${fileId}`, this.logger))
-        .then(data => {
-            return {
-                name: data.name,
-                webUrl: data.webUrl,
-                packageType: data.package ? data.package.type : '',
-                parentReference: {
-                    path: data.parentReference.path,
-                },
-            }
-        });
+        return super.getFileById(`${this.ROOT_URL}/me/drive/items/${fileId}`);
     }
 
     getPublicUrl(fileId) {
@@ -109,13 +99,9 @@ class OneDriveClient {
             .then(file => file.webUrl);
     }
 
-    getPreview(fileId) {
+    getPreview(_driveId, fileId) {
         this.logger.info('Getting OneDrive file preview', { fileId });
-        return this.graphApi.request(`https://graph.microsoft.com/v1.0/me/drive/items/${fileId}/thumbnails`)
-            .catch(logErrorAndReject(`Non-200 while querying file ${fileId}`, this.logger))
-            .then(data => {
-                return  data.value;
-            });
+        return super.getPreview(`${this.ROOT_URL}/me/drive/items/${fileId}/thumbnails`);
     }
 
     createFolder(folderName, parentId = rootFolderId, autorename = true) {
